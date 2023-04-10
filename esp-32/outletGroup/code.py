5bb2a55270245c5ba045862cb38bfc22cc115c26
Aleksandr Tagunov <tagunovalexander@gmail.com>
2023-04-09 19:25:56 -0700

    Add handle all topic

--- esp-32/outletGroup/code.py
+++ esp-32/outletGroup/code.py
@@ -16,7 +16,7 @@ pins = {}
 for x in switches:
     pin =  digitalio.DigitalInOut(getattr(board, switches[x]))
     pin.direction = digitalio.Direction.OUTPUT
-    pin.value = False
+    pin.value = True
     pins[switches[x]] = pin
 
 
@@ -29,8 +29,19 @@ except ImportError:
 mqtt_topic = 'mush/switch-group'
 
 def connected(client, userdata, flags, rc): 
+        print("connected")
         client.subscribe(mqtt_topic + '/set')
+        client.subscribe(mqtt_topic + '/setall')
     
+def handleAll(cient, topic, message):
+        print(pins)
+        for pin in pins:
+            if(message == 'off'):
+                print(pins[pin].value)
+                pins[pin].value = True
+            if(message == 'on'):
+                print(pins[pin].value)
+                pins[pin].value = False
 
 def message(client, topic, message):
     print("New message on topic {0}: {1}".format(topic, message))
@@ -60,10 +71,16 @@ mqtt_client = MQTT.MQTT(
 )
 mqtt_client.on_connect = connected
 mqtt_client.on_message = message
-# mqtt_client.on_disconnected = disconnected
+mqtt_client.add_topic_callback('mush/switch-group/setall', handleAll)
 mqtt_client.connect()
 
 
 while True:
-    mqtt_client.loop()
+    try:
+        mqtt_client.loop()
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        wifi.reset()
+        mqtt_client.reconnect()
+        continue    
     time.sleep(1)
