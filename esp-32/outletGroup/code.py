2622825eeec9292b9db7e5c434e27c8de95c2dd7
Aleksandr Tagunov <tagunovalexander@gmail.com>
2023-04-16 17:07:21 -0700

    Add handle btn press

--- esp-32/outletGroup/code.py
+++ esp-32/outletGroup/code.py
@@ -1,73 +1,87 @@
-import board 
-import digitalio 
+import board
+import digitalio
 import time
 import wifi
 import socketpool
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 import json
+import supervisor
 
 switches = {
     'hum': 'A0',
     'heat': 'A1',
-    'fan' : 'A2',
+    'fan': 'A2',
     'light': 'A3'
-};
+}
 pins = {}
 for x in switches:
-    pin =  digitalio.DigitalInOut(getattr(board, switches[x]))
+    pin = digitalio.DigitalInOut(getattr(board, switches[x]))
     pin.direction = digitalio.Direction.OUTPUT
     pin.value = True
     pins[switches[x]] = pin
 
+btn = digitalio.DigitalInOut(board.BOOT0)
+print(board.BOOT0)
+btn.direction = digitalio.Direction.INPUT
+btn.pull = digitalio.Pull.UP
 
-try: 
+prev_btn_state = btn.value
+btn_i = 0
+
+
+try:
     import secrets
 except ImportError:
     print("Error importing secrets")
-    raise    
+    raise
 
 mqtt_topic = 'mush/switch-group'
 
-def connected(client, userdata, flags, rc): 
-        print("connected")
-        client.subscribe(mqtt_topic + '/set')
-        client.subscribe(mqtt_topic + '/setall')
-    
+
+def connected(client, userdata, flags, rc):
+    print("connected")
+    client.subscribe(mqtt_topic + '/set')
+    client.subscribe(mqtt_topic + '/setall')
+
+
 def handleAll(cient, topic, message):
-        print(pins)
-        for pin in pins:
-            if(message == 'off'):
-                print(pins[pin].value)
-                pins[pin].value = True
-            if(message == 'on'):
-                print(pins[pin].value)
-                pins[pin].value = False
+    for pin in pins:
+        if (message == 'off'):
+            print(pins[pin].value)
+            pins[pin].value = True
+        if (message == 'on'):
+            print(pins[pin].value)
+            pins[pin].value = False
+
+
+def handlBtnPress():
+    for pin in pins:
+        pins[pin].value = not pins[pin].value
+
 
 def message(client, topic, message):
     print("New message on topic {0}: {1}".format(topic, message))
-    try :
+    try:
         msg = json.loads(message)
     except ValueError:
         print("Error parsing message")
-    if('switch' in msg and msg['switch'] in switches):
-        if(msg['value'] == 'on'):
-            pins[switches[msg['switch']]].value = False;
-        if(msg['value'] == 'off'):
+    if ('switch' in msg and msg['switch'] in switches):
+        if (msg['value'] == 'on'):
+            pins[switches[msg['switch']]].value = False
+        if (msg['value'] == 'off'):
             print(pins)
-            pins[switches[msg['switch']]].value = True;
-            
-
+            pins[switches[msg['switch']]].value = True
 
 
 wifi.radio.connect(secrets.network_id, secrets.password)
 pool = socketpool.SocketPool(wifi.radio)
 
 mqtt_client = MQTT.MQTT(
-    broker = "192.168.1.106",
-    port= 1883,  
-    username = "pi",
-    password = "boopyou",
-    socket_pool = pool,
+    broker="192.168.1.106",
+    port=1883,
+    username="pi",
+    password="boopyou",
+    socket_pool=pool,
 )
 mqtt_client.on_connect = connected
 mqtt_client.on_message = message
@@ -78,9 +92,13 @@ mqtt_client.connect()
 while True:
     try:
         mqtt_client.loop()
-    except (ValueError, RuntimeError) as e:
-        print("Failed to get data, retrying\n", e)
-        wifi.reset()
-        mqtt_client.reconnect()
-        continue    
-    time.sleep(1)
+        cur_btn_state = btn.value
+        if (cur_btn_state and not prev_btn_state):
+            handlBtnPress()
+        prev_btn_state = cur_btn_state
+    except (ValueError, RuntimeError, OSError, ConnectionError) as e:
+        print("Network error, reconnecting\n", str(e))
+        time.sleep(10)
+        supervisor.reload()
+        continue
+    time.sleep(0.1)
